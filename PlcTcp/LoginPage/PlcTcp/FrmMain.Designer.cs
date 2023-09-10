b94fcb3f30aa5be419d15c620db3b3b660e23839
jicongwei <769182096@qq.com>
2023-09-10 20:49:52 +0800

    1

--- PlcTcp/LoginPage/PlcTcp/FrmMain.Designer.cs
+++ PlcTcp/LoginPage/PlcTcp/FrmMain.Designer.cs
@@ -33,7 +33,6 @@ namespace PlcTcp
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
 			this.tabControl1 = new System.Windows.Forms.TabControl();
 			this.tabPage1 = new System.Windows.Forms.TabPage();
-			this.textShow1 = new PlcTcp.TextShow();
 			this.bindingNavigator1 = new System.Windows.Forms.BindingNavigator(this.components);
 			this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
 			this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
@@ -52,23 +51,51 @@ namespace PlcTcp
 			this.tb_plc_ip = new System.Windows.Forms.TextBox();
 			this.label1 = new System.Windows.Forms.Label();
 			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.button1 = new System.Windows.Forms.Button();
+			this.tb_send_data = new System.Windows.Forms.TextBox();
 			this.btn_startServer = new System.Windows.Forms.Button();
 			this.tb_server_port = new System.Windows.Forms.TextBox();
 			this.label3 = new System.Windows.Forms.Label();
 			this.tb_server_ip = new System.Windows.Forms.TextBox();
 			this.label4 = new System.Windows.Forms.Label();
+			this.tabPage3 = new System.Windows.Forms.TabPage();
+			this.mqtt_tip = new System.Windows.Forms.Label();
+			this.btn_mqtt_start = new System.Windows.Forms.Button();
+			this.btn_mqtt_stop = new System.Windows.Forms.Button();
+			this.tb_mqtt_port = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.cb_mqtt_ip = new System.Windows.Forms.ComboBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.tabPage4 = new System.Windows.Forms.TabPage();
+			this.tb_mqtt_client_tip = new System.Windows.Forms.TextBox();
+			this.tb_account = new System.Windows.Forms.TextBox();
+			this.tb_password = new System.Windows.Forms.TextBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.tb_Client_Link_IP = new System.Windows.Forms.TextBox();
+			this.btn_matt_Connect = new System.Windows.Forms.Button();
+			this.btn_mqtt_disconnect = new System.Windows.Forms.Button();
+			this.tb_connect_port = new System.Windows.Forms.TextBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.textShow1 = new PlcTcp.TextShow();
 			this.textShow2 = new PlcTcp.TextShow();
+			this.btn_publish = new System.Windows.Forms.Button();
 			this.tabControl1.SuspendLayout();
 			this.tabPage1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).BeginInit();
 			this.bindingNavigator1.SuspendLayout();
 			this.tabPage2.SuspendLayout();
+			this.tabPage3.SuspendLayout();
+			this.tabPage4.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// tabControl1
 			// 
 			this.tabControl1.Controls.Add(this.tabPage1);
 			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Controls.Add(this.tabPage3);
+			this.tabControl1.Controls.Add(this.tabPage4);
 			this.tabControl1.Location = new System.Drawing.Point(12, 38);
 			this.tabControl1.Name = "tabControl1";
 			this.tabControl1.SelectedIndex = 0;
@@ -91,17 +118,6 @@ namespace PlcTcp
 			this.tabPage1.TabIndex = 0;
 			this.tabPage1.Text = "Client";
 			this.tabPage1.UseVisualStyleBackColor = true;
-			// 
-			// textShow1
-			// 
-			this.textShow1.DataType = thinger.DataConvertLib.DataType.Float;
-			this.textShow1.Location = new System.Drawing.Point(56, 98);
-			this.textShow1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-			this.textShow1.Name = "textShow1";
-			this.textShow1.Offset = 4;
-			this.textShow1.Size = new System.Drawing.Size(359, 44);
-			this.textShow1.Start = 0;
-			this.textShow1.TabIndex = 6;
 			// 
 			// bindingNavigator1
 			// 
@@ -250,7 +266,7 @@ namespace PlcTcp
 			this.tb_plc_ip.Name = "tb_plc_ip";
 			this.tb_plc_ip.Size = new System.Drawing.Size(150, 26);
 			this.tb_plc_ip.TabIndex = 1;
-			this.tb_plc_ip.Text = "192.168.1.20";
+			this.tb_plc_ip.Text = "192.168.2.20";
 			// 
 			// label1
 			// 
@@ -263,12 +279,14 @@ namespace PlcTcp
 			// 
 			// tabPage2
 			// 
-			this.tabPage2.Controls.Add(this.textShow2);
+			this.tabPage2.Controls.Add(this.button1);
+			this.tabPage2.Controls.Add(this.tb_send_data);
 			this.tabPage2.Controls.Add(this.btn_startServer);
 			this.tabPage2.Controls.Add(this.tb_server_port);
 			this.tabPage2.Controls.Add(this.label3);
 			this.tabPage2.Controls.Add(this.tb_server_ip);
 			this.tabPage2.Controls.Add(this.label4);
+			this.tabPage2.Controls.Add(this.textShow2);
 			this.tabPage2.Location = new System.Drawing.Point(4, 29);
 			this.tabPage2.Name = "tabPage2";
 			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
@@ -276,6 +294,23 @@ namespace PlcTcp
 			this.tabPage2.TabIndex = 1;
 			this.tabPage2.Text = "Server";
 			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(449, 254);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 39);
+			this.button1.TabIndex = 12;
+			this.button1.Text = "发送";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click_1);
+			// 
+			// tb_send_data
+			// 
+			this.tb_send_data.Location = new System.Drawing.Point(293, 260);
+			this.tb_send_data.Name = "tb_send_data";
+			this.tb_send_data.Size = new System.Drawing.Size(100, 26);
+			this.tb_send_data.TabIndex = 11;
 			// 
 			// btn_startServer
 			// 
@@ -293,7 +328,7 @@ namespace PlcTcp
 			this.tb_server_port.Name = "tb_server_port";
 			this.tb_server_port.Size = new System.Drawing.Size(150, 26);
 			this.tb_server_port.TabIndex = 8;
-			this.tb_server_port.Text = "2001";
+			this.tb_server_port.Text = "2000";
 			// 
 			// label3
 			// 
@@ -310,7 +345,7 @@ namespace PlcTcp
 			this.tb_server_ip.Name = "tb_server_ip";
 			this.tb_server_ip.Size = new System.Drawing.Size(150, 26);
 			this.tb_server_ip.TabIndex = 6;
-			this.tb_server_ip.Text = "192.168.1.241";
+			this.tb_server_ip.Text = "192.168.2.241";
 			// 
 			// label4
 			// 
@@ -320,6 +355,214 @@ namespace PlcTcp
 			this.label4.Size = new System.Drawing.Size(84, 20);
 			this.label4.TabIndex = 5;
 			this.label4.Text = "本机-IP地址";
+			// 
+			// tabPage3
+			// 
+			this.tabPage3.Controls.Add(this.mqtt_tip);
+			this.tabPage3.Controls.Add(this.btn_mqtt_start);
+			this.tabPage3.Controls.Add(this.btn_mqtt_stop);
+			this.tabPage3.Controls.Add(this.tb_mqtt_port);
+			this.tabPage3.Controls.Add(this.label6);
+			this.tabPage3.Controls.Add(this.cb_mqtt_ip);
+			this.tabPage3.Controls.Add(this.label5);
+			this.tabPage3.Location = new System.Drawing.Point(4, 29);
+			this.tabPage3.Name = "tabPage3";
+			this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage3.Size = new System.Drawing.Size(1034, 668);
+			this.tabPage3.TabIndex = 2;
+			this.tabPage3.Text = "MQTTServer";
+			this.tabPage3.UseVisualStyleBackColor = true;
+			// 
+			// mqtt_tip
+			// 
+			this.mqtt_tip.AutoSize = true;
+			this.mqtt_tip.Location = new System.Drawing.Point(37, 107);
+			this.mqtt_tip.Name = "mqtt_tip";
+			this.mqtt_tip.Size = new System.Drawing.Size(0, 20);
+			this.mqtt_tip.TabIndex = 13;
+			// 
+			// btn_mqtt_start
+			// 
+			this.btn_mqtt_start.Location = new System.Drawing.Point(620, 36);
+			this.btn_mqtt_start.Name = "btn_mqtt_start";
+			this.btn_mqtt_start.Size = new System.Drawing.Size(75, 31);
+			this.btn_mqtt_start.TabIndex = 12;
+			this.btn_mqtt_start.Text = "Start";
+			this.btn_mqtt_start.UseVisualStyleBackColor = true;
+			this.btn_mqtt_start.Click += new System.EventHandler(this.btn_mqtt_start_Click);
+			// 
+			// btn_mqtt_stop
+			// 
+			this.btn_mqtt_stop.Location = new System.Drawing.Point(741, 36);
+			this.btn_mqtt_stop.Name = "btn_mqtt_stop";
+			this.btn_mqtt_stop.Size = new System.Drawing.Size(75, 31);
+			this.btn_mqtt_stop.TabIndex = 11;
+			this.btn_mqtt_stop.Text = "Stop";
+			this.btn_mqtt_stop.UseVisualStyleBackColor = true;
+			this.btn_mqtt_stop.Click += new System.EventHandler(this.btn_mqtt_stop_Click);
+			// 
+			// tb_mqtt_port
+			// 
+			this.tb_mqtt_port.Location = new System.Drawing.Point(449, 45);
+			this.tb_mqtt_port.Name = "tb_mqtt_port";
+			this.tb_mqtt_port.Size = new System.Drawing.Size(100, 26);
+			this.tb_mqtt_port.TabIndex = 10;
+			this.tb_mqtt_port.Text = "1883";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(332, 47);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(85, 20);
+			this.label6.TabIndex = 9;
+			this.label6.Text = "Server Port:";
+			// 
+			// cb_mqtt_ip
+			// 
+			this.cb_mqtt_ip.FormattingEnabled = true;
+			this.cb_mqtt_ip.Location = new System.Drawing.Point(137, 39);
+			this.cb_mqtt_ip.Name = "cb_mqtt_ip";
+			this.cb_mqtt_ip.Size = new System.Drawing.Size(121, 28);
+			this.cb_mqtt_ip.TabIndex = 8;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(37, 47);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(70, 20);
+			this.label5.TabIndex = 7;
+			this.label5.Text = "Server IP:";
+			// 
+			// tabPage4
+			// 
+			this.tabPage4.Controls.Add(this.btn_publish);
+			this.tabPage4.Controls.Add(this.tb_mqtt_client_tip);
+			this.tabPage4.Controls.Add(this.tb_account);
+			this.tabPage4.Controls.Add(this.tb_password);
+			this.tabPage4.Controls.Add(this.label9);
+			this.tabPage4.Controls.Add(this.label10);
+			this.tabPage4.Controls.Add(this.tb_Client_Link_IP);
+			this.tabPage4.Controls.Add(this.btn_matt_Connect);
+			this.tabPage4.Controls.Add(this.btn_mqtt_disconnect);
+			this.tabPage4.Controls.Add(this.tb_connect_port);
+			this.tabPage4.Controls.Add(this.label7);
+			this.tabPage4.Controls.Add(this.label8);
+			this.tabPage4.Location = new System.Drawing.Point(4, 29);
+			this.tabPage4.Name = "tabPage4";
+			this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage4.Size = new System.Drawing.Size(1034, 668);
+			this.tabPage4.TabIndex = 3;
+			this.tabPage4.Text = "MQTTClient";
+			this.tabPage4.UseVisualStyleBackColor = true;
+			// 
+			// tb_mqtt_client_tip
+			// 
+			this.tb_mqtt_client_tip.Location = new System.Drawing.Point(63, 150);
+			this.tb_mqtt_client_tip.Name = "tb_mqtt_client_tip";
+			this.tb_mqtt_client_tip.Size = new System.Drawing.Size(100, 26);
+			this.tb_mqtt_client_tip.TabIndex = 24;
+			// 
+			// tb_account
+			// 
+			this.tb_account.Location = new System.Drawing.Point(180, 92);
+			this.tb_account.Name = "tb_account";
+			this.tb_account.Size = new System.Drawing.Size(100, 26);
+			this.tb_account.TabIndex = 23;
+			this.tb_account.Text = "admin";
+			// 
+			// tb_password
+			// 
+			this.tb_password.Location = new System.Drawing.Point(483, 92);
+			this.tb_password.Name = "tb_password";
+			this.tb_password.Size = new System.Drawing.Size(100, 26);
+			this.tb_password.TabIndex = 22;
+			this.tb_password.Text = "123456";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(366, 94);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(95, 20);
+			this.label9.TabIndex = 21;
+			this.label9.Text = "Mqtt Passwd";
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(59, 92);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(102, 20);
+			this.label10.TabIndex = 20;
+			this.label10.Text = "Mqtt Account";
+			// 
+			// tb_Client_Link_IP
+			// 
+			this.tb_Client_Link_IP.Location = new System.Drawing.Point(180, 36);
+			this.tb_Client_Link_IP.Name = "tb_Client_Link_IP";
+			this.tb_Client_Link_IP.Size = new System.Drawing.Size(100, 26);
+			this.tb_Client_Link_IP.TabIndex = 19;
+			this.tb_Client_Link_IP.Text = "127.0.0.1";
+			// 
+			// btn_matt_Connect
+			// 
+			this.btn_matt_Connect.Location = new System.Drawing.Point(654, 27);
+			this.btn_matt_Connect.Name = "btn_matt_Connect";
+			this.btn_matt_Connect.Size = new System.Drawing.Size(75, 31);
+			this.btn_matt_Connect.TabIndex = 18;
+			this.btn_matt_Connect.Text = "Connect";
+			this.btn_matt_Connect.UseVisualStyleBackColor = true;
+			this.btn_matt_Connect.Click += new System.EventHandler(this.btn_matt_Connect_Click);
+			// 
+			// btn_mqtt_disconnect
+			// 
+			this.btn_mqtt_disconnect.Location = new System.Drawing.Point(775, 27);
+			this.btn_mqtt_disconnect.Name = "btn_mqtt_disconnect";
+			this.btn_mqtt_disconnect.Size = new System.Drawing.Size(96, 31);
+			this.btn_mqtt_disconnect.TabIndex = 17;
+			this.btn_mqtt_disconnect.Text = "Disconnect";
+			this.btn_mqtt_disconnect.UseVisualStyleBackColor = true;
+			this.btn_mqtt_disconnect.Click += new System.EventHandler(this.btn_mqtt_disconnect_Click);
+			// 
+			// tb_connect_port
+			// 
+			this.tb_connect_port.Location = new System.Drawing.Point(483, 36);
+			this.tb_connect_port.Name = "tb_connect_port";
+			this.tb_connect_port.Size = new System.Drawing.Size(100, 26);
+			this.tb_connect_port.TabIndex = 16;
+			this.tb_connect_port.Text = "1883";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(366, 38);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(100, 20);
+			this.label7.TabIndex = 15;
+			this.label7.Text = "Connect Port:";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(71, 38);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(85, 20);
+			this.label8.TabIndex = 13;
+			this.label8.Text = "Connect IP:";
+
+			// 
+			// textShow1
+			// 
+			this.textShow1.DataType = thinger.DataConvertLib.DataType.Float;
+			this.textShow1.Location = new System.Drawing.Point(56, 98);
+			this.textShow1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.textShow1.Name = "textShow1";
+			this.textShow1.Offset = 4;
+			this.textShow1.Size = new System.Drawing.Size(359, 43);
+			this.textShow1.Start = 0;
+			this.textShow1.TabIndex = 6;
 			// 
 			// textShow2
 			// 
@@ -331,7 +574,16 @@ namespace PlcTcp
 			this.textShow2.Size = new System.Drawing.Size(479, 73);
 			this.textShow2.Start = 0;
 			this.textShow2.TabIndex = 10;
-			this.textShow2.Load += new System.EventHandler(this.textShow2_Load);
+			// 
+			// btn_publish
+			// 
+			this.btn_publish.Location = new System.Drawing.Point(654, 81);
+			this.btn_publish.Name = "btn_publish";
+			this.btn_publish.Size = new System.Drawing.Size(75, 31);
+			this.btn_publish.TabIndex = 25;
+			this.btn_publish.Text = "publish";
+			this.btn_publish.UseVisualStyleBackColor = true;
+			this.btn_publish.Click += new System.EventHandler(this.btn_publish_Click);
 			// 
 			// FrmMain
 			// 
@@ -353,6 +605,10 @@ namespace PlcTcp
 			this.bindingNavigator1.PerformLayout();
 			this.tabPage2.ResumeLayout(false);
 			this.tabPage2.PerformLayout();
+			this.tabPage3.ResumeLayout(false);
+			this.tabPage3.PerformLayout();
+			this.tabPage4.ResumeLayout(false);
+			this.tabPage4.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -386,5 +642,28 @@ namespace PlcTcp
 		private System.Windows.Forms.TextBox tb_server_ip;
 		private System.Windows.Forms.Label label4;
 		private TextShow textShow2;
+		private System.Windows.Forms.TextBox tb_send_data;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TabPage tabPage3;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.ComboBox cb_mqtt_ip;
+		private System.Windows.Forms.TextBox tb_mqtt_port;
+		private System.Windows.Forms.Button btn_mqtt_start;
+		private System.Windows.Forms.Button btn_mqtt_stop;
+		private System.Windows.Forms.Label mqtt_tip;
+		private System.Windows.Forms.TabPage tabPage4;
+		private System.Windows.Forms.TextBox tb_Client_Link_IP;
+		private System.Windows.Forms.Button btn_matt_Connect;
+		private System.Windows.Forms.Button btn_mqtt_disconnect;
+		private System.Windows.Forms.TextBox tb_connect_port;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.TextBox tb_account;
+		private System.Windows.Forms.TextBox tb_password;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.TextBox tb_mqtt_client_tip;		
+		private System.Windows.Forms.Button btn_publish;
 	}
 }
--- PlcTcp/LoginPage/PlcTcp/FrmMain.cs
+++ PlcTcp/LoginPage/PlcTcp/FrmMain.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Server;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,7 +22,23 @@ namespace PlcTcp
 		public FrmMain()
 		{
 			InitializeComponent();
-		
+			//获取所有ip
+			var Ips = Dns.GetHostAddressesAsync(Dns.GetHostName());
+			List<string> iplist = new List<string>();
+			iplist.Add("127.0.0.1");
+			foreach (IPAddress iP in Ips.Result)
+			{
+				if (iP.AddressFamily == AddressFamily.InterNetwork)
+				{
+					iplist.Add(iP.ToString());
+				}
+			}
+			if (iplist.Count > 0)
+			{
+				this.cb_mqtt_ip.DataSource = iplist;
+				this.cb_mqtt_ip.SelectedIndex = 0;
+			}
+			this.tabControl1.SelectedIndex = 2;
 		}
 		#region 客户端程序
 		//创建Socket;
@@ -169,6 +188,7 @@ namespace PlcTcp
 			}
 
 		}
+		List<Socket> clients = new List<Socket>();
 
 		private void ListenConnect()
 		{
@@ -176,16 +196,18 @@ namespace PlcTcp
 			{
 				Socket tcpClient = TcpServer.Accept();
 				Task.Run(new Action(()=> {
+					clients.Add(tcpClient);
 					ReciveFromPlc(tcpClient);
 				}));
 			}
 		}
-
+		byte[] SetBuffer;
 		private void ReciveFromPlc(Socket tcpClient)
 		{
 			while (true)
 			{
 				byte[] buffer = new byte[1024 * 10];
+				
 				int length = -1;
 				try
 				{
@@ -195,9 +217,10 @@ namespace PlcTcp
 				{
 					break;
 				}
-				if (length == 100)
+				if (length == 110)
 				{
-					byte[] result = ByteArrayLib.GetByteArrayFromByteArray(buffer, 0, length);		
+					byte[] result = ByteArrayLib.GetByteArrayFromByteArray(buffer, 0, length);
+					SetBuffer = ByteArrayLib.GetByteArrayFromByteArray(buffer,100,10);
 					Invoke(new Action(() =>
 					{
 						foreach (Control item in this.tabPage2.Controls)
@@ -212,11 +235,208 @@ namespace PlcTcp
 			}
 			
 		}
+		private void button1_Click_1(object sender, EventArgs e)
+		{
+			if (clients.Count > 0)
+			{
+				float val = Convert.ToSingle(this.tb_send_data.Text);
+				byte[] b = ByteArrayLib.GetByteArrayFromFloat(val);
+				byte[] set = ByteArrayLib.SetByteArray(SetBuffer, val, 0,0);
+				int send = clients[0].Send(set);
+			}
+		}
+
 		#endregion
 
-		private void textShow2_Load(object sender, EventArgs e)
+
+		#region MQTT Server
+
+		private IMqttServer mqttServer;
+		MqttFactory _mqttFactory;
+		MqttFactory mqttFactory {
+			get {
+				if (_mqttFactory == null)
+					_mqttFactory = new MqttFactory();
+				return _mqttFactory;
+			}
+		}
+		private void btn_mqtt_start_Click(object sender, EventArgs e)
+		{
+			if (mqttServer != null)
+				return;
+			mqttServer = mqttFactory.CreateMqttServer();
+			var optionBuilder = new MqttServerOptionsBuilder();
+			//允许多少个同时连接
+			optionBuilder.WithConnectionBacklog(10);
+			optionBuilder.WithDefaultEndpointPort(Convert.ToInt32(this.tb_mqtt_port.Text));
+			optionBuilder.WithDefaultEndpointBoundIPAddress(IPAddress.Parse(this.cb_mqtt_ip.Text));
+			var option = optionBuilder.Build() as MqttServerOptions;
+
+			//添加验证
+			option.ConnectionValidator += (MqttConnectionValidatorContext context) => {
+				if (context.Username == "admin" && context.Password == "123456")
+				{
+					context.ReturnCode = MQTTnet.Protocol.MqttConnectReturnCode.ConnectionAccepted;
+				}
+				else
+				{
+					context.ReturnCode = MQTTnet.Protocol.MqttConnectReturnCode.ConnectionRefusedIdentifierRejected;
+				}
+			};
+
+			mqttServer.ClientConnected += MqttServer_ClientConnected;
+			mqttServer.ClientDisconnected += MqttServer_ClientDisconnected;
+			mqttServer.ClientSubscribedTopic += MqttServer_ClientSubscribedTopic;
+			mqttServer.ClientUnsubscribedTopic += MqttServer_ClientUnsubscribedTopic;
+			mqttServer.ApplicationMessageReceived += MqttServer_ApplicationMessageReceived;
+			mqttServer.Started += MqttServer_Started;
+			mqttServer.Stopped += MqttServer_Stopped;
+			mqttServer.StartAsync(option);
+		}
+
+		private void btn_mqtt_stop_Click(object sender, EventArgs e)
+		{
+			if (mqttServer == null)
+				return;
+			mqttServer.StopAsync();
+			mqttServer = null;
+		}
+
+		private void MqttServer_Stopped(object sender, EventArgs e)
+		{
+			MqttTip("服务器停止");		
+		}
+
+		private void MqttServer_Started(object sender, EventArgs e)
+		{
+			MqttTip("服务器开启");
+		}
+
+		private void MqttServer_ApplicationMessageReceived(object sender, MqttApplicationMessageReceivedEventArgs e)
+		{
+		
+		}
+
+		private void MqttServer_ClientUnsubscribedTopic(object sender, MQTTnet.Server.MqttClientUnsubscribedTopicEventArgs e)
+		{
+		
+		}
+
+		private void MqttServer_ClientSubscribedTopic(object sender, MQTTnet.Server.MqttClientSubscribedTopicEventArgs e)
+		{
+		}
+
+		private void MqttServer_ClientDisconnected(object sender, MQTTnet.Server.MqttClientDisconnectedEventArgs e)
+		{
+		
+		}
+
+		private void MqttServer_ClientConnected(object sender, MQTTnet.Server.MqttClientConnectedEventArgs e)
 		{
 
 		}
+
+		private void MqttTip(string tip)
+		{
+			this.mqtt_tip.Text = tip;
+		}
+		#endregion
+
+		#region MQTT Client
+		IMqttClient mqttClient;
+		private void btn_matt_Connect_Click(object sender, EventArgs e)
+		{
+			if (mqttClient != null)
+				return;
+			mqttClient = mqttFactory.CreateMqttClient();
+			var option = new MqttClientOptions() { ClientId = Guid.NewGuid().ToString("D") };
+			option.ChannelOptions = new MqttClientTcpOptions()
+			{
+				Server = this.tb_Client_Link_IP.Text,
+				Port = Convert.ToInt32(this.tb_connect_port.Text)
+			};
+			option.Credentials = new MqttClientCredentials()
+			{
+				Username = tb_account.Text,
+				Password = tb_password.Text
+			};
+			option.CleanSession = true;
+			option.KeepAlivePeriod = TimeSpan.FromSeconds(100);
+			option.KeepAliveSendInterval = TimeSpan.FromSeconds(10000);
+			mqttClient.Connected += MqttClient_Connected;
+			mqttClient.Disconnected += MqttClient_Disconnected; ;
+			mqttClient.ConnectAsync(option);
+		}
+
+		private void MqttClient_Disconnected(object sender, MQTTnet.Client.MqttClientDisconnectedEventArgs e)
+		{
+			MqttClientTip("断开连接");
+		}
+
+		private void MqttClient_Connected(object sender, MQTTnet.Client.MqttClientConnectedEventArgs e)
+		{
+			MqttClientTip("连接成功");
+		}
+
+		private void MqttClientTip(string tip)
+		{			
+			Invoke(new Action(()=>{
+				this.tb_mqtt_client_tip.Text = tip;
+			}));
+		}
+		private void btn_mqtt_disconnect_Click(object sender, EventArgs e)
+		{
+			if (mqttClient == null)
+				return;
+			mqttClient.DisconnectAsync();
+			mqttClient = null;
+		}
+
+		private void publishTimer_Tick(object sender, EventArgs e)
+		{
+			
+		}
+
+		/// <summary>
+		/// 向服务器某个订阅(Topic)发送消息
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btn_publish_Click(object sender, EventArgs e)
+		{
+			MqttApplicationMessage message = new MqttApplicationMessage()
+			{
+				//Topic = 某个订阅名,
+				Topic = "thinger",
+				Payload = Encoding.UTF8.GetBytes(DateTime.Now.Second.ToString()),
+				QualityOfServiceLevel = MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce,
+				Retain = false
+			};
+
+			mqttClient.PublishAsync(message);
+		}
+
+		xktComm.SiemensS7 objD7 = new xktComm.SiemensS7();
+
+
+		/// <summary>
+		/// 连接西蒙子
+		/// </summary>
+		public void LinkSiemensS7()
+		{
+			objD7.Connect("127.0.0.1",xktComm.Common.CPU_Type.S7200SMART, 0, 0);
+		}
+
+		/// <summary>
+		/// 读取西蒙子的值
+		/// </summary>
+		/// <returns></returns>
+		public string readSiemensS7()
+		{
+			return objD7.Read("DB1.DBD0", xktComm.Common.VarType.Real).ToString();
+		}
+		#endregion
+
+
 	}
 }
