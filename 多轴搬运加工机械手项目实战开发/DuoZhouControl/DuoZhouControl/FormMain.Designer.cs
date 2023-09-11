172e3e88b629c4702f7f06eddbb230db99247a58
jicongwei <769182096@qq.com>
2023-09-11 17:52:05 +0800

    图片资源

--- 多轴搬运加工机械手项目实战开发/DuoZhouControl/DuoZhouControl/FormMain.Designer.cs
+++ 多轴搬运加工机械手项目实战开发/DuoZhouControl/DuoZhouControl/FormMain.Designer.cs
@@ -29,10 +29,19 @@ namespace DuoZhouControl
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+			this.SuspendLayout();
+			// 
+			// FormMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "FormMain";
 			this.Text = "FormMain";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
