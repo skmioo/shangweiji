bc162545ecaf4bae4712dd83bee125f3d7ef420a
jicongwei <769182096@qq.com>
2023-09-12 13:27:51 +0800

    1

--- 多轴搬运加工机械手项目实战开发/DuoZhouControl/DuoZhouControl/FormMain.Designer.cs
+++ 多轴搬运加工机械手项目实战开发/DuoZhouControl/DuoZhouControl/FormMain.Designer.cs
@@ -30,20 +30,301 @@ namespace DuoZhouControl
 		private void InitializeComponent()
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem7 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem8 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem9 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem10 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem11 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem12 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem13 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem14 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem15 = new System.Windows.Forms.ToolStripMenuItem();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.toolStripStatusLabel5 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.toolStripStatusLabel6 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.toolStripStatusLabel7 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.toolStripStatusLabel8 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.DockMain = new WeifenLuo.WinFormsUI.Docking.DockPanel();
+			this.menuStrip1.SuspendLayout();
+			this.statusStrip1.SuspendLayout();
 			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem13,
+            this.toolStripMenuItem10,
+            this.toolStripMenuItem7,
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem1});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Padding = new System.Windows.Forms.Padding(15, 2, 0, 2);
+			this.menuStrip1.Size = new System.Drawing.Size(1264, 29);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// toolStripMenuItem1
+			// 
+			this.toolStripMenuItem1.AutoSize = false;
+			this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3});
+			this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+			this.toolStripMenuItem1.Size = new System.Drawing.Size(90, 25);
+			this.toolStripMenuItem1.Text = "系统帮助(&H)";
+			// 
+			// toolStripMenuItem2
+			// 
+			this.toolStripMenuItem2.AutoSize = false;
+			this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+			this.toolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+			this.toolStripMenuItem2.Text = "重启系统(&R)";
+			// 
+			// toolStripMenuItem3
+			// 
+			this.toolStripMenuItem3.AutoSize = false;
+			this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+			this.toolStripMenuItem3.Size = new System.Drawing.Size(180, 22);
+			this.toolStripMenuItem3.Text = "注销系统(&E)";
+			// 
+			// toolStripMenuItem4
+			// 
+			this.toolStripMenuItem4.AutoSize = false;
+			this.toolStripMenuItem4.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem5,
+            this.toolStripMenuItem6});
+			this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+			this.toolStripMenuItem4.Size = new System.Drawing.Size(90, 25);
+			this.toolStripMenuItem4.Text = "用户管理(&U)";
+			// 
+			// toolStripMenuItem5
+			// 
+			this.toolStripMenuItem5.AutoSize = false;
+			this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+			this.toolStripMenuItem5.Size = new System.Drawing.Size(180, 22);
+			this.toolStripMenuItem5.Text = "重启系统(&R)";
+			// 
+			// toolStripMenuItem6
+			// 
+			this.toolStripMenuItem6.AutoSize = false;
+			this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+			this.toolStripMenuItem6.Size = new System.Drawing.Size(180, 22);
+			this.toolStripMenuItem6.Text = "注销系统(&E)";
+			// 
+			// toolStripMenuItem7
+			// 
+			this.toolStripMenuItem7.AutoSize = false;
+			this.toolStripMenuItem7.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem8,
+            this.toolStripMenuItem9});
+			this.toolStripMenuItem7.Name = "toolStripMenuItem7";
+			this.toolStripMenuItem7.Size = new System.Drawing.Size(90, 25);
+			this.toolStripMenuItem7.Text = "统计分析(&A)";
+			// 
+			// toolStripMenuItem8
+			// 
+			this.toolStripMenuItem8.AutoSize = false;
+			this.toolStripMenuItem8.Name = "toolStripMenuItem8";
+			this.toolStripMenuItem8.Size = new System.Drawing.Size(180, 22);
+			this.toolStripMenuItem8.Text = "重启系统(&R)";
+			// 
+			// toolStripMenuItem9
+			// 
+			this.toolStripMenuItem9.AutoSize = false;
+			this.toolStripMenuItem9.Name = "toolStripMenuItem9";
+			this.toolStripMenuItem9.Size = new System.Drawing.Size(180, 22);
+			this.toolStripMenuItem9.Text = "注销系统(&E)";
+			// 
+			// toolStripMenuItem10
+			// 
+			this.toolStripMenuItem10.AutoSize = false;
+			this.toolStripMenuItem10.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem11,
+            this.toolStripMenuItem12});
+			this.toolStripMenuItem10.Name = "toolStripMenuItem10";
+			this.toolStripMenuItem10.Size = new System.Drawing.Size(90, 25);
+			this.toolStripMenuItem10.Text = "系统设置(&S)";
+			// 
+			// toolStripMenuItem11
+			// 
+			this.toolStripMenuItem11.AutoSize = false;
+			this.toolStripMenuItem11.Name = "toolStripMenuItem11";
+			this.toolStripMenuItem11.Size = new System.Drawing.Size(180, 22);
+			this.toolStripMenuItem11.Text = "重启系统(&R)";
+			// 
+			// toolStripMenuItem12
+			// 
+			this.toolStripMenuItem12.AutoSize = false;
+			this.toolStripMenuItem12.Name = "toolStripMenuItem12";
+			this.toolStripMenuItem12.Size = new System.Drawing.Size(180, 22);
+			this.toolStripMenuItem12.Text = "注销系统(&E)";
+			// 
+			// toolStripMenuItem13
+			// 
+			this.toolStripMenuItem13.AutoSize = false;
+			this.toolStripMenuItem13.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem14,
+            this.toolStripMenuItem15});
+			this.toolStripMenuItem13.Name = "toolStripMenuItem13";
+			this.toolStripMenuItem13.Size = new System.Drawing.Size(90, 25);
+			this.toolStripMenuItem13.Text = "系统管理(&X)";
+			// 
+			// toolStripMenuItem14
+			// 
+			this.toolStripMenuItem14.AutoSize = false;
+			this.toolStripMenuItem14.Name = "toolStripMenuItem14";
+			this.toolStripMenuItem14.Size = new System.Drawing.Size(180, 22);
+			this.toolStripMenuItem14.Text = "重启系统(&R)";
+			// 
+			// toolStripMenuItem15
+			// 
+			this.toolStripMenuItem15.AutoSize = false;
+			this.toolStripMenuItem15.Name = "toolStripMenuItem15";
+			this.toolStripMenuItem15.Size = new System.Drawing.Size(180, 22);
+			this.toolStripMenuItem15.Text = "注销系统(&E)";
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel8,
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel4,
+            this.toolStripStatusLabel5,
+            this.toolStripStatusLabel6,
+            this.toolStripStatusLabel7});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 707);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(1264, 22);
+			this.statusStrip1.TabIndex = 1;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// toolStripStatusLabel1
+			// 
+			this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+			this.toolStripStatusLabel1.Size = new System.Drawing.Size(372, 17);
+			this.toolStripStatusLabel1.Text = "                                                                                 " +
+    "          ";
+			// 
+			// toolStripStatusLabel2
+			// 
+			this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+			this.toolStripStatusLabel2.Size = new System.Drawing.Size(59, 17);
+			this.toolStripStatusLabel2.Text = "登录用户:";
+			// 
+			// toolStripStatusLabel3
+			// 
+			this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+			this.toolStripStatusLabel3.Size = new System.Drawing.Size(68, 17);
+			this.toolStripStatusLabel3.Text = "【管理员】";
+			// 
+			// toolStripStatusLabel4
+			// 
+			this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+			this.toolStripStatusLabel4.Size = new System.Drawing.Size(16, 17);
+			this.toolStripStatusLabel4.Text = "  ";
+			// 
+			// toolStripStatusLabel5
+			// 
+			this.toolStripStatusLabel5.Name = "toolStripStatusLabel5";
+			this.toolStripStatusLabel5.Size = new System.Drawing.Size(59, 17);
+			this.toolStripStatusLabel5.Text = "系统时间:";
+			// 
+			// toolStripStatusLabel6
+			// 
+			this.toolStripStatusLabel6.Name = "toolStripStatusLabel6";
+			this.toolStripStatusLabel6.Size = new System.Drawing.Size(126, 17);
+			this.toolStripStatusLabel6.Text = "2023/09/12 12:00:00";
+			// 
+			// toolStripStatusLabel7
+			// 
+			this.toolStripStatusLabel7.Name = "toolStripStatusLabel7";
+			this.toolStripStatusLabel7.Size = new System.Drawing.Size(115, 17);
+			this.toolStripStatusLabel7.Text = "系统版本：【v1.0】";
+			// 
+			// toolStripStatusLabel8
+			// 
+			this.toolStripStatusLabel8.Name = "toolStripStatusLabel8";
+			this.toolStripStatusLabel8.Size = new System.Drawing.Size(372, 17);
+			this.toolStripStatusLabel8.Text = "                                                                                 " +
+    "          ";
+			// 
+			// DockMain
+			// 
+			this.DockMain.ActiveAutoHideContent = null;
+			this.DockMain.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.DockMain.DocumentStyle = WeifenLuo.WinFormsUI.Docking.DocumentStyle.DockingWindow;
+			this.DockMain.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World);
+			this.DockMain.Location = new System.Drawing.Point(0, 29);
+			this.DockMain.Name = "DockMain";
+			this.DockMain.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.DockMain.Size = new System.Drawing.Size(1264, 678);
+			this.DockMain.TabIndex = 2;
 			// 
 			// FormMain
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(1264, 729);
+			this.Controls.Add(this.DockMain);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.menuStrip1);
+			this.Font = new System.Drawing.Font("黑体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MainMenuStrip = this.menuStrip1;
 			this.Name = "FormMain";
-			this.Text = "FormMain";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "多轴控制系统";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem13;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem14;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem15;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem10;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem11;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem12;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem7;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem8;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem9;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel5;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel6;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel8;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel7;
+		private WeifenLuo.WinFormsUI.Docking.DockPanel DockMain;
 	}
 }
